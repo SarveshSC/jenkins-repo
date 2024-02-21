2d645ae8d128dd8eabdbb529e99264cab1e26a35
SarveshSC <sarveshchoudhary8402@gmail.com>
2024-02-21 11:56:32 +0530

    Initial Commit

---
+++ src/main/java/com/hexaware/restcrud/RestCrudMysqlApplication.java
@@ -0,0 +1,16 @@
+package com.hexaware.restcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCrudMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCrudMysqlApplication.class, args);
+
+		System.out.println("Javeed updated RestCrud main() ....");
+
+	}
+
+}
